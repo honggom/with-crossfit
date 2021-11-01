a52fecd7c64f7b6b15923738a98e78c4a7fc818d
honggom <cavok699@naver.com>
2021-11-01 09:48:03 +0900

    init: BE init

---
+++ BE/src/main/java/hong/gom/withcrossfit/WithCrossfitApplication.java
@@ -0,0 +1,13 @@
+package hong.gom.withcrossfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithCrossfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithCrossfitApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/hong/gom/withcrossfit/WithCrossfitApplicationTests.java
@@ -0,0 +1,13 @@
+package hong.gom.withcrossfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithCrossfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
