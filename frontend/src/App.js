6fd652425b351b037dcea0b75c8d7f7cf1d5e0c0
honggom <cavok699@naver.com>
2021-11-04 17:59:08 +0900

    temp: 임시 커밋

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,28 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home, Login, Logout } from './pages';
+import Nav from './component/Nav/Nav';
+import styles from './App.module.css';
+import { isMobile, isBrowser } from 'react-device-detect';
 
 export default function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} exact />
-          <Route path="/logout" element={<Logout />} exact />
-          <Route path="/home" element={<Home />} exact />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+
+    console.log(isBrowser);
+    console.log(isMobile);
+
+    return (
+        <div className={styles.app}>
+            <section>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Login />} exact />
+                        <Route path="/logout" element={<Logout />} exact />
+                        <Route path="/home" element={<Home />} exact />
+                    </Routes>
+                </BrowserRouter>
+            </section>
+            <nav className={styles.nav}>
+                <Nav />
+            </nav>
+        </div>
+    );
 }
---
+++ frontend/src/component/Wod.js
@@ -0,0 +1,7 @@
+export default function Wod() {
+    return (
+        <div>
+            <h1>WOD</h1>
+        </div>
+    );
+}
